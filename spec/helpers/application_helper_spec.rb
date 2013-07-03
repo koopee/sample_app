f4c5c191ae13f82367d281181e90e96e23717749
Kari-Pekka Pyylampi <koopeepyy@gmail.com>
2013-07-03 09:41:17 +0300

    application test helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,7 +9,7 @@ describe ApplicationHelper do
       expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
     end
     it "should not include a bar for the home page" do
-      expect(full_title("")).not_to match(/|/)
+      expect(full_title("")).not_to match(/\|/)
     end
   end
 end
