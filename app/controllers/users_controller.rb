3204ca69d1e8e6a85708b86fc5ba42f92d1ce8dd
Kari-Pekka Pyylampi <koopeepyy@gmail.com>
2013-07-06 09:08:06 +0300

    Välikommitti

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
+  def index
+    @users = User.all
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -7,6 +12,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update user_params
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   def create
     @user = User.new( user_params )
     if @user.save
@@ -22,5 +39,17 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
