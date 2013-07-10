00696ea250c53bec50c6001aceec5a06e4fd56e3
Kari-Pekka Pyylampi <koopeepyy@gmail.com>
2013-07-10 12:51:14 +0300

    User microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def new
     @user = User.new
@@ -49,16 +50,9 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:admin, :name, :email, :password, :password_confirmation)
     end
-    
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-    
+        
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
